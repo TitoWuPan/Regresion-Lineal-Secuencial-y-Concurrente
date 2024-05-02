d5c75f2d78075d7eaa5f7233dd1be82383240f36
Tito Wu <91169600+TitoWuPan@users.noreply.github.com>
2024-05-01 19:10:44 -0500

    Add files via upload

---
+++ Resultados/MediaRecortada.py
@@ -0,0 +1,27 @@
+Secuencial = []
+Concurrente = []
+
+with open('1. SecuencialResultado.txt', 'r') as f:
+    for linea in f:
+        valor = int(linea.strip())  
+        Secuencial.append(valor)
+
+valores_ordenados = sorted(Secuencial)
+
+valores_recortados = valores_ordenados[50:-50]
+media_recortada = sum(valores_recortados) / len(valores_recortados)
+
+print("Media recortada Secuencial:", media_recortada)
+
+
+with open('2. ConcurrenteResultado.txt', 'r') as f:
+    for linea in f:
+        valor = int(linea.strip())  
+        Concurrente.append(valor)
+
+valores_ordenados = sorted(Concurrente)
+
+valores_recortados = valores_ordenados[50:-50]
+media_recortada = sum(valores_recortados) / len(valores_recortados)
+
+print("Media recortada Concurrente:", media_recortada)
